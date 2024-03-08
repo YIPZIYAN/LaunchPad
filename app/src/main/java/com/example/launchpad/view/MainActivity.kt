89526c8c94e8ebb1e8c047d4b9e58748549f9cb4
YIPZIYAN <103913961+YIPZIYAN@users.noreply.github.com>
2024-03-08 15:45:17 +0800

    done bottom navigation

--- app/src/main/java/com/example/launchpad/view/MainActivity.kt
+++ app/src/main/java/com/example/launchpad/view/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.example.launchpad.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.launchpad.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
         setContentView(R.layout.activity_user) //change accordingly for testing
+        startActivity(Intent(this,UserActivity::class.java))
     }
 }
--- app/src/main/java/com/example/launchpad/view/UserActivity.kt
+++ app/src/main/java/com/example/launchpad/view/UserActivity.kt
@@ -2,11 +2,19 @@ package com.example.launchpad.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.launchpad.R
+import com.example.launchpad.databinding.ActivityUserBinding
 
 class UserActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityUserBinding
+    private val nav by lazy { supportFragmentManager.findFragmentById(R.id.user_nav_host)!!.findNavController() }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user)
+        binding = ActivityUserBinding.inflate(layoutInflater)
+
+        binding.bottomNavigation.setupWithNavController(nav)
+        setContentView(binding.root)
     }
 }
