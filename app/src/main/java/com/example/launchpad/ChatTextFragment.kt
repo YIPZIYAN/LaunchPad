8ba9af9d3ad0bd5b8efb912c83689358f5c70f41
GohChunYen <chunyengoh030503@gmail.com>
2024-03-29 11:04:43 +0800

    ChatFragment UI

---
+++ app/src/main/java/com/example/launchpad/ChatTextFragment.kt
@@ -0,0 +1,30 @@
+package com.example.launchpad
+
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ChatTextFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = ChatTextFragment()
+    }
+
+    private val viewModel: ChatTextViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // TODO: Use the ViewModel
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_chat_text, container, false)
+    }
+}
---
+++ app/src/main/java/com/example/launchpad/ChatTextViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.launchpad
+
+import androidx.lifecycle.ViewModel
+
+class ChatTextViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
