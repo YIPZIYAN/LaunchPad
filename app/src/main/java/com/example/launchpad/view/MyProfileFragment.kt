6bba385f8f420b00f1b38cd02e136356e408e1e3
YIPZIYAN <103913961+YIPZIYAN@users.noreply.github.com>
2024-04-07 15:32:11 +0800

    profile job

--- app/src/main/java/com/example/launchpad/view/MyProfileFragment.kt
+++ app/src/main/java/com/example/launchpad/view/MyProfileFragment.kt
@@ -6,10 +6,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.lifecycle.Lifecycle
 import androidx.navigation.fragment.findNavController
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.launchpad.R
 import com.example.launchpad.databinding.FragmentMyProfileBinding
 import com.example.launchpad.viewmodel.MyProfileViewModel
+import com.google.android.material.tabs.TabLayoutMediator
 import io.getstream.avatarview.coil.loadImage
 
 class MyProfileFragment : Fragment() {
@@ -20,6 +25,10 @@ class MyProfileFragment : Fragment() {
 
     private lateinit var viewModel: MyProfileViewModel
     private lateinit var binding: FragmentMyProfileBinding
+    private val tabItems = arrayOf(
+        "Job",
+        "Post"
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -34,12 +43,37 @@ class MyProfileFragment : Fragment() {
                     findNavController().navigate(R.id.action_profileFragment_to_settingFragment)
                     true
                 }
+
                 else -> false
             }
         }
 
+        val adapter = ViewPagerAdapter(requireActivity().supportFragmentManager, lifecycle)
+        binding.viewPager.adapter = adapter
+
+
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            tab.text = tabItems[position]
+        }.attach()
         return binding.root
     }
+
+    class ViewPagerAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
+        FragmentStateAdapter(fragmentManager, lifecycle) {
+
+        override fun getItemCount(): Int {
+            return 2
+        }
+
+        override fun createFragment(position: Int): Fragment {
+            when (position) {
+                0 -> return TabMyJobFragment()
+                1 -> return TabMyJobFragment()
+                else -> throw Exception()
+            }
+        }
+    }
+
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
