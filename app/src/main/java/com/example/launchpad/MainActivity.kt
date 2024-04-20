e2b6b22628a776b32d698632095772775013a15e
YIPZIYAN <103913961+YIPZIYAN@users.noreply.github.com>
2024-04-20 23:16:29 +0800

    google login

--- app/src/main/java/com/example/launchpad/MainActivity.kt
+++ app/src/main/java/com/example/launchpad/MainActivity.kt
@@ -8,11 +8,10 @@ import com.example.launchpad.R
 import com.example.launchpad.auth.viewmodel.LoginViewModel
 
 class MainActivity : AppCompatActivity() {
-    private val vm: LoginViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
         setContentView(R.layout.activity_main) //change accordingly for testing
-        vm.init()
+
     }
 }
--- app/src/main/java/com/example/launchpad/auth/view/LoginFragment.kt
+++ app/src/main/java/com/example/launchpad/auth/view/LoginFragment.kt
@@ -1,15 +1,25 @@
 package com.example.launchpad.auth.view
 
+import android.app.Activity
+import android.content.Intent
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.example.launchpad.viewmodel.LoginViewModel
 import com.example.launchpad.R
+import com.example.launchpad.auth.viewmodel.LoginViewModel
 import com.example.launchpad.databinding.FragmentLoginBinding
+import com.example.launchpad.view.MyProfileFragment
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.Firebase
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.auth
 
 class LoginFragment : Fragment() {
 
@@ -20,6 +30,8 @@ class LoginFragment : Fragment() {
 
     private lateinit var viewModel: LoginViewModel
     private lateinit var binding: FragmentLoginBinding
+    private val auth = Firebase.auth
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -38,7 +50,46 @@ class LoginFragment : Fragment() {
             findNavController().navigate(R.id.action_loginFragment_to_forgotPasswordFragment)
         }
 
+        binding.btnGoogle.setOnClickListener { signInWithGoogle() }
+
         return binding.root
+    }
+
+    private fun signInWithGoogle() {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.client_id))
+            .requestEmail()
+            .build()
+
+        val googleSignInClient = GoogleSignIn.getClient(requireActivity(), gso)
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, Activity.RESULT_OK)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == Activity.RESULT_OK) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                Log.d("error", "onActivityResult: error")
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    findNavController().navigate(R.id.action_loginFragment_to_userActivity)
+                    requireActivity().finish()
+                } else {
+                }
+            }
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
