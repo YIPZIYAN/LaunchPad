0d0e23f85b4b4c0c7624fea45b7a35b720ed8fee
YIPZIYAN <103913961+YIPZIYAN@users.noreply.github.com>
2024-05-03 21:53:36 +0800

    sign out for email verfification

--- app/src/main/java/com/example/launchpad/EmailVerificationActivity.kt
+++ app/src/main/java/com/example/launchpad/EmailVerificationActivity.kt
@@ -1,17 +1,11 @@
 package com.example.launchpad
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.lifecycle.lifecycleScope
 import com.example.launchpad.auth.viewmodel.SignUpViewModel
-import com.example.launchpad.data.viewmodel.UserViewModel
 import com.example.launchpad.databinding.ActivityEmailVerificationBinding
 import com.example.launchpad.util.intentWithoutBackstack
-import kotlinx.coroutines.launch
 
 class EmailVerificationActivity : AppCompatActivity() {
     lateinit var binding: ActivityEmailVerificationBinding
@@ -32,6 +26,11 @@ class EmailVerificationActivity : AppCompatActivity() {
             if (it) {
                 intentWithoutBackstack(this, UserActivity::class.java)
             }
+        }
+
+        binding.btnLogout.setOnClickListener {
+            viewModel.auth.signOut()
+            intentWithoutBackstack(this, MainActivity::class.java)
         }
 
         binding.btnResend.setOnClickListener { viewModel.sendEmailVerification() }
